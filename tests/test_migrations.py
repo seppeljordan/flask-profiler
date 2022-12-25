2f905e9daf0dd7e51e9fd324a7852e7d423a6fd0
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-12-25 18:36:40 +0100

    Fix unit tests

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -7,8 +7,5 @@ from flask_profiler.storage.sqlite.migrations import Migrations
 class MigrationTests(TestCase):
     def test_can_run_migrations(self) -> None:
         connection = sqlite3.connect(":memory:")
-        cursor = connection.cursor()
-        migrations = Migrations()
-        for migration in migrations.get_relevant_versions(0):
-            migration.run(cursor)
-        connection.commit()
+        migrations = Migrations(connection)
+        migrations.run_necessary_migrations()
